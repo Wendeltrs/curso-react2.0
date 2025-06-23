914f1c9ae4f78c6019dbdeb3e9b09af34ac6cd6e
Wendeltrs <wendeljt17@gmail.com>
2025-06-23 15:14:05 -0300

    feat: add light theme and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes/Light";
 
 export const App = () => {
   return (
-    <BrowserRouter
-      future={{
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
